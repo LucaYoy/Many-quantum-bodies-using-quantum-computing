95d40002155a24f36736c2028ab8aadff0db2e06
NicholasSynesi <117459444+NicholasSynesi@users.noreply.github.com>
2022-12-16 12:40:24 +0000

    Add files via upload
    Brickwall via tensordot with fixes

--- Nicholas/myproject/BrickWallTensorDot.py
+++ Nicholas/myproject/BrickWallTensorDot.py
@@ -7,10 +7,7 @@ Created on Thu Dec  1 15:13:20 2022
 
 import numpy as np
 import Gates as g
-from scipy.stats import unitary_group as U
-
-U = U.rvs(4)
-U = U.reshape(2,2,2,2)
+import randomunitary as rd
 
 class Circuit:
     def __init__(self,n):
@@ -24,15 +21,32 @@ def had(qu,Circuit):#Hadamard gate
     Circuit.psi = np.tensordot(g.Had,Circuit.psi,(1,qu))
     Circuit.psi = np.moveaxis(Circuit.psi,0,qu)
         
-def CNOT(qu,Circuit):#CNOT gate
+def cNOT(qu,Circuit):#CNOT gate
     Circuit.psi = np.tensordot(g.CNOTt,Circuit.psi,((2,3),(qu,qu+1)))
     Circuit.psi = np.moveaxis(Circuit.psi,(0,1),(qu,qu+1))
 
 def unitary(qu,Circuit):#random unitary
-    Circuit.psi = np.tensordot(U,Circuit.psi,((2,3),(qu,qu+1)))
+    Circuit.psi = np.tensordot(g.Ut,Circuit.psi,((2,3),(qu,qu+1)))
+    Circuit.psi = np.moveaxis(Circuit.psi,(0,1),(qu,qu+1))
+    
+def unitarycsv(qu,Circuit,M):
+    Circuit.psi = np.tensordot(rd.matrices[M],Circuit.psi,((2,3),(qu,qu+1)))
     Circuit.psi = np.moveaxis(Circuit.psi,(0,1),(qu,qu+1))
 
-
+def brick_wall(M, num_gates, Circuit):
+    gate = 0
+    for i in range(M):
+        for j in range(0, Circuit.n,2):
+            if gate >= num_gates:
+                return
+            unitarycsv(j, Circuit, gate)
+            gate += 1
+        for k in range(i%2+1, Circuit.n-1,2):
+            if gate >= num_gates:
+                return
+            unitarycsv(k, Circuit, gate)
+            gate += 1   
+      
 def solutionT(Circuit):
     #had(0,Circuit)
     for i in range(Circuit.n%2,Circuit.n-1,2):
@@ -40,24 +54,26 @@ def solutionT(Circuit):
     for j in range(Circuit.n%2+1,Circuit.n-1,2):
       unitary(j,Circuit)
     
-def solver(M,Circuit):
-    for i in range(M):
-        solutionT(Circuit)
-
 Circuit=Circuit(4)
-solver(2,Circuit)      
-print(Circuit.psi.flatten())   
-t = Circuit.psi.reshape(4,4)
+brick_wall(2,5,Circuit)   
+t = Circuit.psi.reshape(4,4) 
 
-#class Circuit2:
-#    def __init__(self,n):
-#        self.n = n
-#        self.psi = np.zeros(2**n)
-#        self.psi[0] = 1
-#        self.psi = self.psi.reshape(tuple([2]*n))#define initial psi
+class Circuit2:
+    def __init__(self,n):
+        self.n = n
+        self.psi = np.zeros(2**n)
+        self.psi[0] = 1
+        self.psi = self.psi.reshape(tuple([2]*n))#define initial psi
 
-#def solutionM(Circuit2):
-    #Had(0,Circuit2)
+def solutionM(Circuit2):
+    unitarycsv(0,Circuit2,0)
+    unitarycsv(2,Circuit2,1)
+    unitarycsv(1,Circuit2,2)
+    unitarycsv(0,Circuit2,3)
+    unitarycsv(2,Circuit2,4)
+    #unitarycsv(1,Circuit2,5)
+    #unitarycsv(0,Circuit2,3)
+    #unitarycsv(2,Circuit2,4)
     #Unitary(0,Circuit2)
     #Unitary(2,Circuit2)
     #Unitary(4,Circuit2)
@@ -66,10 +82,10 @@ t = Circuit.psi.reshape(4,4)
     #Unitary(2,Circuit2)
     #Unitary(1,Circuit2)
     
-#Circuit2=Circuit2(4)
-#solutionM(Circuit2)
+Circuit2=Circuit2(4)
+solutionM(Circuit2)
 #print(Circuit2.psi.flatten()) 
-#r = Circuit2.psi.reshape(4,4)
+r = Circuit2.psi.reshape(4,4)
 
 
 
