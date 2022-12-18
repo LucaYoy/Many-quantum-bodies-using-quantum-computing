5fdf9fed13369b7953f77f32a36ee8f4afded754
LucaYoy <lucaion781@gmail.com>
2022-12-18 13:23:32 +0200

    implemented function to remove gates

--- Luca/Coding and Latex/Python/BrickWall_51.py
+++ Luca/Coding and Latex/Python/BrickWall_51.py
@@ -1,17 +1,43 @@
 import numpy as np
-import usefulGates as g
 from functools import reduce
+from scipy.stats import unitary_group as U
 
-def tensordotPsiWithTwoQubitGate(psi,twoQubitGateWithIndices): return np.moveaxis(np.tensordot(twoQubitGateWithIndices[0],psi,axes = ([2,3],[twoQubitGateWithIndices[1],twoQubitGateWithIndices[2]])),[0,1],[twoQubitGateWithIndices[1],twoQubitGateWithIndices[2]])
+class BrickWallCircuit:
+	def __init__(self,N,M,psiIn = None,reverseOrder = 0,gates = None):
+		self.N = N
+		self.M = M
+		self.gates = gates
+		self.reverseOrder = reverseOrder
+		self.nrGates = int(np.ceil(M/2) + M)
 
-def computeAnyCircuitUsingTensorDot(psi, gatesWithQubitsIndices): return reduce(tensordotPsiWithTwoQubitGate,gatesWithQubitsIndices,psi)
+		if gates is None: 
+			gates = [U.rvs(4).reshape(2,2,2,2) for i in range(self.nrGates)]
+		self.gates = gates
 
-def brickWall(N,M,psi, gates, startOddLayer = 0, reverseOrder = 0): #start loop over layers on odd layer if startOddLayer = 1, i.e starting with one gate on that layer
-	counter = 0
-	gatesWithQubitsIndices = []
-	for i in range(startOddLayer,M+startOddLayer): #decorate gate in argument list with qubit numbers on which it, acts to make it suitable for use with above function
-		gatesSlice = gates[counter:counter + int(np.floor((N-i%2)/2))]
-		gatesWithQubitsIndices += [(gate,j,j+1) for gate, j in zip(gatesSlice[::(-1)**reverseOrder], range(i%2,N-1,2))]
-		counter += int(np.floor((N-i%2)/2))
-	#print(gatesWithQubitsIndices)
-	return computeAnyCircuitUsingTensorDot(psi, gatesWithQubitsIndices)
+		if psiIn is None:
+			psi = np.zeros(2**N)
+			psi[0]  = 1
+			psi = psi.reshape(tuple([2]*N))
+			psiIn = psi
+		self.psiIn = psiIn
+		
+	def gatesWithQubitsIndices(self, MOriginal = 0): #MOriginal only needed when we want to reverse order, since we need it in order to determine if we start on an odd or even layer, otherwise its value its irrelevant
+		counter = 0
+		gatesWithQubitsIndices = []
+		layerStart = self.reverseOrder and (MOriginal+1)%2
+		gates = self.gates[::(-1)**self.reverseOrder]
+
+		for i in range(layerStart,self.M+layerStart): #decorate gates with qubit numbers on which they act
+			gatesSlice = gates[counter:counter + int(np.floor((self.N-i%2)/2))]
+			gatesWithQubitsIndices += [(gate,j,j+1) for gate, j in zip(gatesSlice[::(-1)**self.reverseOrder], range(i%2,self.N-1,2))]
+			counter += int(np.floor((self.N-i%2)/2))
+		return gatesWithQubitsIndices
+
+	#if we rverse order then we need to contract [0,1] indices of gates
+	def tensordotPsiWithTwoQubitGate(self,psi,twoQubitGateWithIndices): return np.moveaxis(np.tensordot(twoQubitGateWithIndices[0],psi,axes = ([2*(1-self.reverseOrder),2*(1-self.reverseOrder)+1],[twoQubitGateWithIndices[1],twoQubitGateWithIndices[2]])),[0,1],[twoQubitGateWithIndices[1],twoQubitGateWithIndices[2]])
+
+	#allow for custom addition of gates with qubit indices needed for when we remove a gate, otherwise if no argument added just use the inbuild method to calculate these
+	def computeUsingTensorDot(self, gatesWithQubitsIndices = None, MOriginal = 0):
+		if gatesWithQubitsIndices is None: 
+			gatesWithQubitsIndices = self.gatesWithQubitsIndices(MOriginal)
+		return reduce(self.tensordotPsiWithTwoQubitGate,gatesWithQubitsIndices,self.psiIn)
--- Luca/Coding and Latex/Python/checkBrickWall_52.py
+++ Luca/Coding and Latex/Python/checkBrickWall_52.py
@@ -17,7 +17,7 @@ solutionTD = bw.brickWall(N, M, psiReshaped, UReshaped).flatten()
 print(solutionTD,'\n -----')
 solutionKN = k.computeCircuitGate(psi,k.computeSliceGate(N,(U[0],0,1),(U[1],2,3)),k.computeSliceGate(N,(U[2],1,2)),k.computeSliceGate(N,(U[3],0,1),(U[4],2,3)),k.computeSliceGate(N,(U[5],1,2)))
 print(solutionKN,'\n -----')
-solutionTD2 = bw.computeAnyCircuitUsingTensorDot(psiReshaped,[(UReshaped[0],0,1),(UReshaped[1],2,3),(UReshaped[2],1,2),(UReshaped[3],0,1),(UReshaped[4],2,3),(UReshaped[5],1,2)]).flatten()
+solutionTD2 = bw.computeCircuitUsingTensorDot(psiReshaped,[(UReshaped[0],0,1),(UReshaped[1],2,3),(UReshaped[2],1,2),(UReshaped[3],0,1),(UReshaped[4],2,3),(UReshaped[5],1,2)]).flatten()
 print(solutionTD2, '\n -----') 
 
 #print (solutionTD == solutionKN) #why gives false??
