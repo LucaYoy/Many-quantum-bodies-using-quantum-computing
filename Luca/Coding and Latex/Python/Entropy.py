b788f7eff4797de0a3785defeb8fc8cde6adbb74
LucaYoy <lucaion781@gmail.com>
2023-02-18 14:51:10 +0000

    implemented both entropy methods

--- Luca/Coding and Latex/Python/Entropy.py
+++ Luca/Coding and Latex/Python/Entropy.py
@@ -1,11 +1,17 @@
 import numpy as np
 
-def computeEntropy(n,psiTarget):
+def computeEntropy(n,psiTarget,alternative = False):
 	nrQubits = len(psiTarget.shape)
-	qubitsToContract = range(nrQubits)[n:]
-	rhoA = np.tensordot(psiTarget, np.conjugate(psiTarget), (qubitsToContract,qubitsToContract)) #partial state
-	eigenvalues = np.linalg.eigh(rhoA.reshape((2**n,2**n)))[0] #eigenvalues of partial state
-	eigenvalues = [eigenvalue for eigenvalue in eigenvalues if eigenvalue>10**(-12)]
-	entropy = -np.sum(eigenvalues*np.log(eigenvalues))
+	if alternative:
+		qubitsToContract = range(nrQubits)[n:]
+		rhoA = np.tensordot(psiTarget, np.conjugate(psiTarget), (qubitsToContract,qubitsToContract)) #partial state
+		eigenvalues = np.linalg.eigh(rhoA.reshape((2**n,2**n)))[0] #eigenvalues of partial state
+		eigenvalues = [eigenvalue for eigenvalue in eigenvalues if eigenvalue>10**(-12)]
+		entropy = -np.sum(eigenvalues*np.log(eigenvalues))
+	else:
+		psiReshaped = psiTarget.reshape(2**n,2**(nrQubits-n))
+		eigenvalues = np.linalg.svd(psiReshaped)[1]**2
+		eigenvalues = [eigenvalue for eigenvalue in eigenvalues if eigenvalue>10**(-12)]
+		entropy = -np.sum(eigenvalues*np.log(eigenvalues))
 
 	return entropy
--- Luca/Coding and Latex/Python/OptimizeCircuit_7.py
+++ Luca/Coding and Latex/Python/OptimizeCircuit_7.py
@@ -3,9 +3,14 @@ import BrickWall_51 as bw
 import ExactDiag_21 as ed
 import Entropy as en
 
-circuit = bw.BrickWallCircuit(3, 8)
+circuit = bw.BrickWallCircuit(5, 8)
 psiTarget = ed.exactDiag(circuit.N, 1.5, 1)[1].reshape(tuple([2]*circuit.N))
-#psiTarget = (1/np.sqrt(2))*np.array([1,0,0,1]).reshape(2,2)
-print(en.computeEntropy(1, psiTarget))
+#psiTarget = (1/np.sqrt(2))*np.array([1,0,0,1]).reshape(2,2) #bell state
+entropyMethod1 = en.computeEntropy(4, psiTarget)
+entropyMethod2 = en.computeEntropy(4, psiTarget,True)
+
+print(entropyMethod2, entropyMethod1)
+if np.abs(entropyMethod1-entropyMethod2)>0.01:
+	raise Exception("something wrong")
 
 #circuit.optimize(psiTarget,0.001,1000)
