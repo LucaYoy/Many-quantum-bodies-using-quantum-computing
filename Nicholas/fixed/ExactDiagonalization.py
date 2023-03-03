f03a9fbeb075a59bff3dee71d31ce1c304eb15b6
NicholasSynesi <117459444+NicholasSynesi@users.noreply.github.com>
2023-03-03 20:28:15 +0000

    Fixed exact diagonalization
    Made changes to my exact diagonalization function, as it was causing my graph for the entanglement entropy to not be symmetric

--- Nicholas/fixed/ExactDiagonalization.py
+++ Nicholas/fixed/ExactDiagonalization.py
@@ -5,21 +5,25 @@ def exactDiagonalization(n,J,h):
     
     H = np.zeros((2**n,2**n))
     
+    sum1 = 0
+    sum2 = 0
+    
     for i in range(1,n):
         
-        H += -J * np.kron(np.kron(np.eye(2**(i-1)),g.X) ,np.kron(g.X,np.eye(2**(n-i-1))))
+        sum1 += -J * np.kron(np.kron(np.eye(2**(i-1)),g.X) ,np.kron(g.X,np.eye(2**(n-i-1))))
         
-        H += h * np.kron(np.eye(2**(i)), np.kron(g.Z, np.eye(2**(n-i-1))))
+    for i in range(1,n+1):
+        
+        sum2 += h * np.kron(np.eye(2**(i-1)), np.kron(g.Z, np.eye(2**(n-i))))
+        
+    H = sum1 + sum2
     
     _, V = np.linalg.eigh(H)
     
     return V[:,0].reshape(tuple([2]*n))
 
-
 if __name__ == "__main__":
 
-    H = exactDiagonalization(3,1,0)
-
-    E,V = np.linalg.eigh(H)
+    p = exactDiagonalization(4, 1, 1)
     
     
