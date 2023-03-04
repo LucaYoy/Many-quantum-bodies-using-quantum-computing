4a9e3ba1e9ce7e45eebfd23ed65646b4915d531a
LucaYoy <lucaion781@gmail.com>
2023-03-04 16:22:04 +0000

    generalized entropy calculation

--- Luca/Coding and Latex/Python/Entropy.py
+++ Luca/Coding and Latex/Python/Entropy.py
@@ -1,17 +1,13 @@
 import numpy as np
 
-def computeEntropy(n,psiTarget,alternative = False):
+def S(A,psiTarget):
 	nrQubits = len(psiTarget.shape)
-	if alternative:
-		qubitsToContract = range(nrQubits)[n:]
-		rhoA = np.tensordot(psiTarget, np.conjugate(psiTarget), (qubitsToContract,qubitsToContract)) #partial state
-		eigenvalues = np.linalg.eigh(rhoA.reshape((2**n,2**n)))[0] #eigenvalues of partial state
-		eigenvalues = [eigenvalue for eigenvalue in eigenvalues if eigenvalue>10**(-12)]
-		entropy = -np.sum(eigenvalues*np.log(eigenvalues))
-	else:
-		psiReshaped = psiTarget.reshape(2**n,2**(nrQubits-n))
-		eigenvalues = np.linalg.svd(psiReshaped)[1]**2
-		eigenvalues = [eigenvalue for eigenvalue in eigenvalues if eigenvalue>10**(-12)]
-		entropy = -np.sum(eigenvalues*np.log(eigenvalues))
+	n = len(A)
+
+	qubitsToContract = list(set(range(nrQubits)) - set(np.array(A)-1))
+	rhoA = np.tensordot(psiTarget, np.conjugate(psiTarget), (qubitsToContract,qubitsToContract)) #partial state
+	eigenvalues = np.linalg.eigh(rhoA.reshape((2**n,2**n)))[0] #eigenvalues of partial state
+	eigenvalues = [eigenvalue for eigenvalue in eigenvalues if eigenvalue>10**(-12)]
+	entropy = -np.sum(eigenvalues*np.log(eigenvalues))
 
 	return entropy
