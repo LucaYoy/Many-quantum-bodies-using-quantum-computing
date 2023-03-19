abda1a0261142633d9fbabd649f72f3b72cdb7d3
LucaYoy <lucaion781@gmail.com>
2023-03-19 15:00:41 +0000

    Managed to use HPC for plots

--- Luca/Coding and Latex/HPC/scripts/main.py
+++ Luca/Coding and Latex/HPC/scripts/main.py
@@ -1,15 +1,18 @@
+import numpy as np
 import sys
-import BrickWall_51 as bw
+#import BrickWall_51 as bw
 import ExactDiag_21 as ed
+import plots_9 as plts
+#import Entropy as en
+#import matplotlib.pyplot as plt
 
 
 which = int(sys.argv[1])
 
-parameters = [{'N':qb,'M':5,'j':1.5,'h':1} for qb in range(1,11)]
+parameters = [{'N':6,'j':1,'h':1,'M':[1,2,3],'maxIterations':1000,'runs':10}, {'N':6,'j':1,'h':1.5,'M':[1,2,3],'maxIterations':1000,'runs':10}]
 param = parameters[which]
-p = f"N = {str(param['N'])} M = {str(param['M'])} j = {str(param['j'])} h = {str(param['h'])}"
 
-with open("../parameters.txt",'a') as file:
-	file.write(p)
-
-print(p)
+N,j,h,layers,maxIterations,runs = param.values()
+psiTarget = ed.exactDiag(N, j, h)[2].reshape(tuple([2]*N))
+plts.plotOvelap_sweeps(psiTarget, j, h, layers, maxIterations, runs)
+#p = f"N = {str(param['N'])} M = {str(param['M'])} j = {str(param['j'])} h = {str(param['h'])}"
