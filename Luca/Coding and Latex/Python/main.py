163b67c92292e65d45a568726f9d84901a5c1c1f
LucaYoy <lucaion781@gmail.com>
2023-03-25 12:01:57 +0000

    .

--- Luca/Coding and Latex/Python/main.py
+++ Luca/Coding and Latex/Python/main.py
@@ -5,7 +5,7 @@ import Entropy as en
 import plots_9 as plts
 
 N = 6
-j,h = 1,1
+j,h = 1,1.5
 exactD = ed.exactDiag(N, j, h)
 psiTarget = exactD[2].reshape(tuple([2]*N))
 exactE = exactD[1]
@@ -16,14 +16,14 @@ H = exactD[0]
 #psiTarget = (1/np.sqrt(2))*np.array([1,0,0,1]).reshape(2,2) #bell state
 
 layers = [1,2,3]
-approxStates = [bw.BrickWallCircuit(N,M,gatesRandomFlag=True).optimize(psiTarget, 0.0001, 1000)[0] for M in layers]
+# approxStates = [bw.BrickWallCircuit(N,M,gatesRandomFlag=True).optimize(psiTarget, 0.0001, 1000)[0] for M in layers]
 
-plts.plotEnergy(psiTarget, approxStates, exactE, H)
-plts.plotOvelap(psiTarget, approxStates)
-plts.plotS(psiTarget, approxStates,layers)
-plts.plotMatrixI(psiTarget, approxStates,layers)
-plts.plotJ(psiTarget, approxStates,layers)
-plts.plotJ(psiTarget, approxStates,layers,log=True)
+# plts.plotEnergy(psiTarget, approxStates, exactE, H)
+# plts.plotOvelap(psiTarget, approxStates)
+# plts.plotS(psiTarget, approxStates,layers)
+# plts.plotMatrixI(psiTarget, approxStates,layers)
+# plts.plotJ(psiTarget, approxStates,layers)
+# plts.plotJ(psiTarget, approxStates,layers,log=True)
 
 approxStatesRand = [[bw.BrickWallCircuit(N,M,gatesRandomFlag=True).optimize(psiTarget, 0.0001, 1000)[1:] for i in range(10)] for M in layers]
 approxStatesId = [[bw.BrickWallCircuit(N,M,gatesRandomFlag=False).optimize(psiTarget, 0.0001, 1000)[1:] for i in range(10)] for M in layers]
