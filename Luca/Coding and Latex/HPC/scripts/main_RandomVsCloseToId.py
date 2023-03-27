69ad360f0bc00dc4306692f651015548ee3dcb29
Adam Smith <adam.smith.home@ntlworld.com>
2023-03-27 11:04:33 +0100

    Fix ExactDiag memory issue and move to sparse

--- Luca/Coding and Latex/HPC/scripts/main_RandomVsCloseToId.py
+++ Luca/Coding and Latex/HPC/scripts/main_RandomVsCloseToId.py
@@ -1,23 +1,37 @@
 import numpy as np
 import sys
 import BrickWall_51 as bw
-import ExactDiag_21 as ed
+import ExactDiag_adam as ed
 #import plots_9 as plts
 import pickle
 #import Entropy as en
 #import matplotlib.pyplot as plt
 
-#sh file array 0-9
-which = int(sys.argv[1])
+import psutil
+process = psutil.Process() # get current process to check memory usage
 
-parameters = [{'N':14,'j':1,'h':1,'g':1,'M':5,'maxIterations':20000,'run':i} for i in range(1,11)]
+#sh file array 0-9
+which = int(sys.argv[1])-1  # -1 because array starts at 0
+
+parameters = [{'N':14,'j':1,'h':1,'g':1,'M':5,'maxIterations':200,'run':i} for i in range(1,11)]
 param = parameters[which]
 
 N,j,h,M,g,maxIterations,run = param.values()
-psiTarget = ed.exactDiag(N,j,h,g)[2].reshape(tuple([2]*N))
+
+psiTarget = ed.exactDiagSparse(N,j,h,g)[2].reshape(tuple([2]*N))
+
+print("psiTarget created")
+print(f"Memory used: {process.memory_info().rss/(1024)**3:.3f} GB") # print memory used to 3 significant figures
 
 approxStateRand = bw.BrickWallCircuit(N,M,gatesRandomFlag=True).optimize(psiTarget, 0.0001, maxIterations,GD=False)[1:]
+
+print("optimisation random done")
+print(f"Memory used: {process.memory_info().rss/(1024)**3:.3f} GB") # print memory used to 3 significant figures
+
 approxStateId = bw.BrickWallCircuit(N,M,gatesRandomFlag=False).optimize(psiTarget, 0.0001, maxIterations,GD=False)[1:]
+
+print("optimisation id done")
+print(f"Memory used: {process.memory_info().rss/(1024)**3:.3f} GB") # print memory used to 3 significant figures
 
 with open(f'approxStateRand{run}.pkl','wb') as f:
 	pickle.dump(approxStateRand,f)
